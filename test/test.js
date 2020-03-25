2c00727f9af8bbb0ada99341d0492341822e3f7e
Yacine Lazaar <ycn.lzr@gmail.com>
2020-03-25 21:56:06 +0100

    testing failing test

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe('Unit testing the / route', function() {
     it('Should return OK status', function() {
       request(app)
 		.get('/')
-		.expect(200)
+		.expect(400)
 		.end(function(err, res) {
 			if (err) throw err;
 		});
